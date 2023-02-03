b5866e3bbc51f654b9816277f777b6f682373c79
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2023-02-03 11:40:36 +0100

    Saving changes

--- playground/src/components/Lego/lego.js
+++ playground/src/components/Lego/lego.js
@@ -1,8 +1,27 @@
 import { Suspense, useState, useRef  } from 'react'
 import { Canvas, createPortal, useFrame, useThree } from '@react-three/fiber'
-import { useGLTF,OrbitControls, useFBO, MeshTransmissionMaterial, Html  } from '@react-three/drei'
+import { useGLTF,OrbitControls, useFBO, MeshTransmissionMaterial,  Points, PointMaterial, Html   } from '@react-three/drei'
 import * as THREE from "three";
 import { easing } from 'maath'
+import * as random from 'maath/random/dist/maath-random.esm'
+
+
+function Stars(props) {
+  const ref = useRef()
+  const [sphere] = useState(() => random.inSphere(new Float32Array(15000), { radius: 4.5 }))
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10
+    ref.current.rotation.y -= delta / 15
+  })
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
+        <PointMaterial transparent color="#add8e6" size={0.085} sizeAttenuation={true} depthWrite={false} />
+      </Points>
+    </group>
+  )
+}
+
   
 
 function Lens({ children, damping = 0.15, ...props }) {
@@ -28,7 +47,7 @@ function Lens({ children, damping = 0.15, ...props }) {
       // The following code will render that scene into a buffer, whose texture will then be fed into
       // a plane spanning the full screen and the lens transmission material
       state.gl.setRenderTarget(buffer)
-      state.gl.setClearColor('#d8d7d7')
+      state.gl.setClearColor('#ccc')
       state.gl.render(scene, state.camera)
       state.gl.setRenderTarget(null)
     })
@@ -39,7 +58,7 @@ function Lens({ children, damping = 0.15, ...props }) {
           <boxGeometry />
           <meshBasicMaterial map={buffer.texture} />
         </mesh>
-        <mesh scale={0.25} ref={ref} rotation-x={Math.PI / 2} geometry={nodes.Cylinder.geometry} {...props}>
+        <mesh scale={0.15} ref={ref} rotation-x={Math.PI / 2} geometry={nodes.Cylinder.geometry} {...props}>
           <MeshTransmissionMaterial buffer={buffer.texture} ior={1.2} thickness={1.5} anisotropy={0.1} chromaticAberration={1.5} />
         </mesh>
       </>
@@ -50,9 +69,12 @@ function Lens({ children, damping = 0.15, ...props }) {
 function Gucci (props) {
     const { nodes, materials } = useGLTF('/right.glb')
     return (
-      <group {...props} dispose={null}>
+      <>
+       <Stars />
+      <group receiveShadow {...props} dispose={null}>
         <mesh geometry={nodes.id_Punta001.geometry} material={materials.Default} position={[0, -1, 0]} rotation={[0, 0, 0]} scale={15} />
       </group>
+      </>
     )
   }
 
@@ -62,22 +84,22 @@ export default function Lego(props) {
     <>
 
 <Canvas camera={{ position: [0, 0, 20], fov: 15 }}  style={{backgroundColor: "black", height: "100vh", width: "100vw"}} >
+
        <Suspense fallback={null}>
       
        <Lens>
-       <pointLight intensity={200}/>
-      <Gucci />
        {/* <Html>
-            <div >
-             GUCCI DON'T RUN
-            </div>
-          </Html> */}
+        <h1>Gucci</h1>
+      </Html> */}
+       {/* <spotLight intensity={1.5}/>
+       <pointLight intensity={2.5} position={[10,0,0]}/> */}
+       <spotLight intensity={60} position={[0.8,0,0.5]}/>
+      <pointLight intensity={100}/> 
+      <Gucci />
+      
        </Lens>
        {/* <OrbitControls /> */}
-       
-       
       </Suspense>
-   
     </Canvas>
 </>
   )
