ba3d86bfe11d41f3c9fb3ab65e3d33e722adcd19
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2023-02-23 20:37:20 +0100

    Ball animation

---
+++ playground/src/components/ball/ball.js
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <div>
+      <Spline style={{backgroundColor: "white" , display: "block" , height: "100vh", width: "100vw"}} scene="https://prod.spline.design/v3x7ai7Ks7nAmy9b/scene.splinecode" />
+    </div>
+  );
+}
