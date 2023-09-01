ee071a67d2853d3fba25b17d009aa3d7df004462
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2023-09-01 18:00:30 +0200

    Falling Gucci

--- playground/src/components/nav/nav.js
+++ playground/src/components/nav/nav.js
@@ -14,6 +14,7 @@ import {
   import Map from '../map/map'
   import Portal from '../portal/portal'
   import Head from '../head/head'
+  import Falling from '../falling/falling'
 
 
   export default function Nav() {
@@ -31,13 +32,14 @@ import {
               <Route path="/map" element={<Map />} />
               <Route path="/portal" element={<Portal />} />
               <Route path="/head" element={<Head />} />
+              <Route path="/falling" element={<Falling />} />
 
           </Routes>
           <header>
             <nav className='App-header'>
               <Link to="/shapes">Nike Jordans particles</Link> 
               <Link to="/water">Floating effect</Link>
-              <Link to="/tensor">Tensorflow</Link>
+              {/* <Link to="/tensor">Tensorflow</Link> */}
               <Link to="/gucci">Gucci</Link>
               <Link to="/lego">Gucci shoe 2.0</Link>
               <Link to="/ball">Ball animation</Link>
@@ -45,6 +47,7 @@ import {
               <Link to="/map">Maps</Link>
               <Link to="/portal">Porral</Link>
               <Link to="/head">Head</Link>
+              <Link to="/falling">Falling</Link>
             </nav>
           </header>
     </Router>  
